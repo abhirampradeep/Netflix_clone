c04f17ec571490ae3fa6a03cb7fff5be9784f37f
abhiram.pradeep <abhiram.pradeep@calpinetech.com>
2023-08-16 11:47:02 +0530

    first

---
+++ lib/splash.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Splash_Screen extends StatefulWidget {
+  const Splash_Screen({super.key});
+
+  @override
+  State<Splash_Screen> createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('ff'),
+      ),
+    );
+  }
+}
